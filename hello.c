c282a0635d5764ca4632846b96ed3d67437cb9f6
Jose Andres Mena <andres.menaarias@gmail.com>
2020-08-31 21:38:48 -0600

    Hello word multiple threads numbered

--- hello.c
+++ hello.c
@@ -1,18 +1,38 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 void* threadRun (void* args) {
-    printf("Hello world from secondary thread\n");
+    printf("Hello world from thread %zu\n", (size_t)args);
     return NULL;
 }
 
-int main(void) {
+int main(int argc, char* arg[]) {
 
-    pthread_t thread;
-    pthread_create(&thread, NULL, threadRun, NULL);
+    // arg[0] = command name
+    // arg[1] = number of threads
+
+    size_t thread_count = 0;
+
+    if (argc >= 2) {
+        thread_count = (size_t)strtoul(arg[1], NULL, 10);
+    } else {
+        fprintf(stderr, "error. Invalid number of parameters.\n");
+        return 1;
+    }
+
+    pthread_t* threads = malloc((size_t)(thread_count * sizeof(pthread_t)));
+    
+    for (size_t i = 0; i < thread_count; ++i) {
+        pthread_create(&threads[i], NULL, threadRun, (void*)i);
+    }
+    
     printf("Hello world from main thread\n");
 
-    pthread_join(thread, NULL);
+
+    for (size_t i = 0; i < thread_count; ++i) {
+        pthread_join(threads[i], NULL);
+    }
 
     return 0;
 }
